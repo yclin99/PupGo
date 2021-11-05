dbf2d6aee4211308d9467e62ac1f34eff95eb506
Hsin-Wei Yu <evelynyu@wifi-131-179-42-163.host.ucla.edu>
2021-11-05 16:36:34 -0700

    Initial Xcode Project

---
+++ PupGo/Shared/PupGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PupGoApp.swift
+//  Shared
+//
+//  Created by Hsin-Wei Yu on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PupGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
