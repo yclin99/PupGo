1caed0596183436f66302590d70cee0dfb44c319
hsinweiyo <hsinweiyo@gmail.com>
2021-11-07 17:48:21 -0800

    Divided main page into four tabs, including matching, exploration, notification, and setting. Started working on Login Page page, currently support Google Login and email login

--- PupGo/Shared/PupGoApp.swift
+++ PupGo/Shared/PupGoApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
 
 @main
 struct PupGoApp: App {
+    // Connecting App Delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            MainView()
+            LoginView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // Initializing Firebase
+        FirebaseApp.configure()
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 }
